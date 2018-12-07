9a6903095b98abb4c24eb4e62c54c119212400b7
Geoffrey Emerson <geoffrey.emerson.git@gmail.com>
2018-12-06 17:04:25 -0800

    prevent modal from updating offscreen

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../../hoc/AAux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li key = {igKey}>
-                    <span style={{textTransform:'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-                </li>
-            );
-        });
+class OrderSummary extends Component {
+    // This could be functional, we only changed it to a class for the componentWillUpdate log.
+    componentWillUpdate() {
+        console.log('>> OrderSummary Will Update <<');
+    }
 
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button buttonType='Danger' clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button buttonType='Success' clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    );
+    render () {
+        let ingredientSummary;
+        try {
+            ingredientSummary = Object.keys(this.props.ingredients)
+                .map(igKey => {
+                    return (
+                        <li key = {igKey}>
+                            <span style={{textTransform:'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+                        </li>
+                    );
+                });
+        } catch (e) {
+            console.log('Error in OrderSummary > ingredientSummary')
+            console.log(e)
+        }
+
+        return (
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
+                <p>Continue to checkout?</p>
+                <Button buttonType='Danger' clicked={this.props.purchaseCancelled}>CANCEL</Button>
+                <Button buttonType='Success' clicked={this.props.purchaseContinued}>CONTINUE</Button>
+            </Aux>
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
